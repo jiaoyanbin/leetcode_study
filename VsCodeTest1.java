3a04ce8aa64102d2a56aecbe80dd365a8614fcfe
jiaoyanbin <jiaoyanbin@tal.com>
2020-10-13 14:29:33 +0800

    first commit

---
+++ VsCodeTest1.java
@@ -0,0 +1,8 @@
+class VsCodeTest1{
+public static void main(String[] args) {
+    for (int i = 0; i < 100; i++) {
+        System.out.println("i = "+i);
+    }
+    System.out.println("你好世界");
+}
+}
